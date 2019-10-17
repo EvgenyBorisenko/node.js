fa0997d3aaf8af72345636ff7593061fd02f8991
EvgenyBorisenko <borisenkoevgeny1991@gmail.com>
2019-10-17 14:54:17 +0300

    create server on mongoDB

---
+++ test-server/models/Todo.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  complited: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = model("Todo", schema);
